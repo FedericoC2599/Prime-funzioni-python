d87e6591c18ec81cc8b34cf3f9c56dc10b3bcafc
Federico Cianci <153202786+FedericoC2599@users.noreply.github.com>
2023-12-15 01:25:45 +0100

    Update Funzioni.py

--- Funzioni.py
+++ Funzioni.py
@@ -1,4 +1,4 @@
-# Queste sono le mie prime funzioni in Python
+# Queste sono funzioni di aritmetica di base
 # Autore: F.Cianci
 
 def sum(a, b):
@@ -56,44 +56,3 @@ def div(a,b):
     else:
         raise ValueError("Non puoi dividere per zero")
     
-def ciao():
-    '''
-    Stampa la stringa ''Ciao sono Federico''
-    
-    Ritorna: None
-    '''
-    print('Ciao sono Federico')
-    
-def mangia_fetta_di_pizza(un_ottavo=True):
-    '''
-    Valutazione della voracità di Federico
-    
-    Parametri:
-        un_ottavo -- Se True Federico non è vorace
-                    Se False Federico sta prendendo una brutta strada
-        
-    Ritorna: None 
-    
-    '''
-    if un_ottavo:
-        print('Sono Federico, sono un ragazzo che mangia sano e la mia fetta di pizza era proporzionata')
-    else:
-        print('Sono Federico e sto diventando un porco come il mio coinquilino Enrico')
-
-def compito(a,b,c=True):
-    '''
-    A seconda del valore del parametro c la funzione moltiplica o dividere
-    
-    Parametri:
-        a -- Numero intero
-        b -- Numero intero
-        c -- Valore booleano, 
-            se True moltiplica a e b,
-            se False divide a e b
-            
-    Ritorna: La moltiplicazone o la divisione di a e b in base al parametro c
-    '''
-    if c:
-        print(mul(a,b))
-    else:
-        print(div(a,b))
